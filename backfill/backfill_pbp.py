1afa3db369a85c6df37cb21d4928f95b40c2d517
Meyappan Subbaiah <msubbaiah@Meyappans-MacBook-Pro.local>
2023-04-23 18:05:58 -0400

    get it working

--- backfill/backfill_pbp.py
+++ backfill/backfill_pbp.py
@@ -1,5 +1,5 @@
 import asyncio
-from time import time
+import time
 from httpx import AsyncClient
 import nba_scraper.wnba_scrape_functions as ws
 import pandas as pd
@@ -9,7 +9,7 @@ import json
 # scrape wnba pbp
 async def scrape(game_id, season):
     game_results = ws.get_wnba_pbp_api(game_id)
-    save_json(game_results, game_id, season)
+    await save_json(game_results, game_id, season)
 
 # save wnba json
 async def save_json(pbp_json, game_id, season):
@@ -19,10 +19,9 @@ async def save_json(pbp_json, game_id, season):
 
 async def main():
     start_time = time.time()
-    print('Saving the output of extracted information')
     # better way to do this by season?
     season = 2022
-    game_ids = list(range(int(f"02{season-2000}00001"), int(f"02{season-2000}00010")))
+    game_ids = list(range(int(f"02{season-2000}00001"), int(f"02{season-2000}00204")))
     tasks = []
     for game in game_ids:
         o_game_id = "0"+str(game)
